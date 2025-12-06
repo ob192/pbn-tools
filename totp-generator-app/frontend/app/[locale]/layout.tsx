e41a8fa6099dcf025f719f895c486896570eac19
C5323037 <oleksandr.bunin@sap.com>
2025-12-06 19:03:15 +0200

    totp-generator-app

--- totp-generator-app/frontend/app/[locale]/layout.tsx
+++ totp-generator-app/frontend/app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import { NextIntlClientProvider } from 'next-intl'
 import { getTranslations } from 'next-intl/server'
 import { notFound } from 'next/navigation';
 import '../globals.css';
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'], display: 'swap' });
 
@@ -95,8 +96,30 @@ export default async function RootLayout({
             <link rel="canonical" href={`${siteUrl}/${locale}`} />
             <link rel="dns-prefetch" href="//fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
+            <Script
+                id="gtm-head"
+                strategy="afterInteractive"
+            >{`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-5DBPC2PV');
+        `}</Script>
+
         </head>
         <body className={`${inter.className} antialiased bg-neutral-950 text-neutral-50`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+            <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-5DBPC2PV"
+                height="0"
+                width="0"
+                style={{ display: 'none', visibility: 'hidden' }}
+            />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <NextIntlClientProvider locale={locale} messages={messages}>
             {children}
         </NextIntlClientProvider>
